5c4092db0dd269a597ab6457f99135060e72c209
sai <coolkrishsai@gmail.com>
2021-02-19 20:04:56 +0530

    second

--- saigithub/ViewController.swift
+++ saigithub/ViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myBTN: UIButton!
     @IBOutlet weak var myLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sai()
         // Do any additional setup after loading the view.
     }
 func sai()
 {}
 
+    
+    
+
+    
 }
 
