28995c6c4ba1d4da1da56f4842f6392f400f6603
sai <coolkrishsai@gmail.com>
2021-02-19 19:57:57 +0530

    initial commit

--- saigithub/ViewController.swift
+++ saigithub/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+func sai()
+{}
 
 }
 
